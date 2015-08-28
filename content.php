7b21cb4cb584310614b9ae110357f5bdbeb9ea56
nasser <nasseralikarimi@gmail.com>
2015-08-28 09:35:14 +0000

    add code in files types: .sql,.php remove code in files types: .php

--- content.php
+++ content.php
@@ -532,5 +532,5 @@
         echo "iroyj";
         echo "wyugu";
         echo "jghga";
-        echo "nyuqw";
+        echo "tdmhg";
 ?>
--- content.sql
+++ content.sql
@@ -912,3 +912,4 @@ SELECT * from qqgng;
 SELECT * from rpgpu;
 SELECT * from itrir;
 SELECT * from pycoc;
+SELECT * from xtega;
