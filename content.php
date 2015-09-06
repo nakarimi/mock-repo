a2de1b910fcc5b4d0e08d5072df5764e22d32004
nasser <nasseralikarimi@gmail.com>
2015-09-06 18:47:57 +0000

    add code in files types: .php,.sql remove code in files types: .php

--- content.php
+++ content.php
@@ -539,5 +539,5 @@
         echo "vvemt";
         echo "ojaee";
         echo "umbpx";
-        echo "jbfbu";
+        echo "vhswn";
 ?>
--- content.sql
+++ content.sql
@@ -922,3 +922,4 @@ SELECT * from rkbwj;
 SELECT * from pyylo;
 SELECT * from hhykx;
 SELECT * from wwfpv;
+SELECT * from yosdo;
