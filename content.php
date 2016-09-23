6820daed73f7787039d43091803ebdab09a86652
nasser <nasseralikarimi@gmail.com>
2016-09-23 17:56:39 +0000

    add code in files types: .css,.php,.sql,.js remove code in files types: .css,.php

--- content.php
+++ content.php
@@ -822,5 +822,5 @@
         echo "ifwew";
         echo "baqld";
         echo "ysueo";
-        echo "wydaj";
+        echo "olhuw";
 ?>
--- content.sql
+++ content.sql
@@ -1610,3 +1610,4 @@ SELECT * from bitws;
 SELECT * from xtkhm;
 SELECT * from krmor;
 SELECT * from mbpdf;
+SELECT * from yvylf;
