57608b932ee2fa8ccba2d97d7d0d3a01644e1acc
nasser <nasseralikarimi@gmail.com>
2023-06-01 07:00:48 +0000

    add code in files types: .css,.js,.php,.sql remove code in files types: .js,.php

--- content.php
+++ content.php
@@ -1,5 +1,5 @@
 <?php
         echo "xbqke";
         echo "hwkol";
-        echo "olmak";
+        echo "udtxr";
 ?>
--- content.sql
+++ content.sql
@@ -2,3 +2,4 @@ SELECT * from gdegb;
 SELECT * from uamlp;
 SELECT * from lxylp;
 SELECT * from ecusp;
+SELECT * from oupuv;
